c72c4e7bfa96e09da6e1602269ff0bf935c94c1a
beasiteri <siteribea@gmail.com>
2026-03-11 19:02:28 +0100

    Use tanstack query

--- packages/client/src/components/reviews/ReviewList.tsx
+++ packages/client/src/components/reviews/ReviewList.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Skeleton from 'react-loading-skeleton';
 import StarRating from './StarRating';
+import { useQuery } from '@tanstack/react-query';
 
 type Props = {
    productId: number;
@@ -15,34 +15,27 @@ type Review = {
    createdAt: string;
 };
 
-type GetReviewsResponses = {
+type GetReviewsResponse = {
    summary: string | null;
    reviews: Review[];
 };
 
 const ReviewList = ({ productId }: Props) => {
-   const [reviewData, setReviewData] = useState<GetReviewsResponses>();
-   const [isLoading, setIsLoading] = useState(false);
-   const [error, setError] = useState('');
+   const {
+      data: reviewData,
+      isLoading,
+      error,
+   } = useQuery<GetReviewsResponse>({
+      queryKey: ['reviews', productId],
+      queryFn: () => fetchReviews(),
+   });
 
-   useEffect(() => {
-      const fetchReviews = async () => {
-         try {
-            setIsLoading(true);
-            const { data } = await axios.get<GetReviewsResponses>(
-               `/api/products/${productId}/reviews`
-            );
-            setReviewData(data);
-         } catch (error) {
-            console.error(error);
-            setError('Could not fetch the review. Try again!');
-         } finally {
-            setIsLoading(false);
-         }
-      };
-
-      fetchReviews();
-   }, [productId]);
+   const fetchReviews = async () => {
+      const { data } = await axios.get<GetReviewsResponse>(
+         `/api/products/${productId}/reviews`
+      );
+      return data;
+   };
 
    if (isLoading) {
       return (
@@ -59,7 +52,9 @@ const ReviewList = ({ productId }: Props) => {
    }
 
    if (error) {
-      return <p className="text-red-500">{error}</p>;
+      return (
+         <p className="text-red-500">Could not fetch reviews. Try again!</p>
+      );
    }
 
    return (
