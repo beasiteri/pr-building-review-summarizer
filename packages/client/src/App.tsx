519d553990645026b54447e9fd9ededb61873166
beasiteri <siteribea@gmail.com>
2026-03-11 17:05:30 +0100

    Display reviews on frontend

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,19 +1,9 @@
-import { useState, useEffect } from 'react';
-import { Button } from './components/ui/button';
+import ReviewList from './components/reviews/ReviewList';
 
 function App() {
-   const [message, setMessage] = useState('');
-
-   useEffect(() => {
-      fetch('api/hello')
-         .then((res) => res.json())
-         .then((data) => setMessage(data.message));
-   });
-
    return (
-      <div className="p-4">
-         <p className="font-bold text-3xl">{message}</p>
-         <Button>Click me</Button>
+      <div className="p-4 h-screen w-full">
+         <ReviewList productId={1} />
       </div>
    );
 }
---
+++ packages/client/src/components/reviews/ReviewList.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+type Props = {
+   productId: number;
+};
+
+type Review = {
+   id: string;
+   author: string;
+   content: string;
+   rating: number;
+   createdAt: string;
+};
+
+type GetReviewsResponses = {
+   summary: string | null;
+   reviews: Review[];
+};
+
+const ReviewList = ({ productId }: Props) => {
+   const [reviewData, setReviewData] = useState<GetReviewsResponses>();
+
+   useEffect(() => {
+      const fetchReviews = async () => {
+         const { data } = await axios.get<GetReviewsResponses>(
+            `/api/products/${productId}/reviews`
+         );
+         setReviewData(data);
+      };
+
+      fetchReviews();
+   }, [productId]);
+
+   return (
+      <div className="flex flex-col gap-5">
+         {reviewData?.reviews.map((review) => (
+            <div key={review.id}>
+               <div className="font-semibold">{review.author}</div>
+               <div>Rating: {review.rating}/5</div>
+               <p className="py-2">{review.content}</p>
+            </div>
+         ))}
+      </div>
+   );
+};
+
+export default ReviewList;
