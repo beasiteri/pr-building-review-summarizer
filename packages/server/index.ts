dcdf0ae8e8bea3c77ea558195b9f4313139da0b7
beasiteri <siteribea@gmail.com>
2026-03-11 09:27:54 +0100

    Refactor - separation of concern

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,21 +1,15 @@
 import express from 'express';
-import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import router from './routes';
 
 // Read all the variables
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(router);
+
 const port = process.env.PORT || 3000;
-
-// Define routes
-app.get('/', (req: Request, res: Response) => {
-   res.send('Hello World!');
-});
-
-app.get('/api/hello', (req: Request, res: Response) => {
-   res.json({ message: 'Hello World!' });
-});
 
 // Start web server
 app.listen(port, () => {
---
+++ packages/server/routes.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import { reviewController } from './controllers/review.controller';
+
+const router = express.Router();
+
+router.get('/api/products/:id/reviews', reviewController.getReviews);
+
+export default router;
